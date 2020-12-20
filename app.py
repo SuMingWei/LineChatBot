b243eed03106d61ccd2daa77ffdad50e5fe2f53b
sumingwei <46164371+SuMingWei@users.noreply.github.com>
2020-12-20 17:58:51 +0800

    [test] state

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from utils import send_text_message
 
 load_dotenv()
 
-
 machine = TocMachine(
     states=["user", "state1", "state2"],
     transitions=[
@@ -104,7 +103,8 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
+            
+            send_text_message(event.reply_token, "k")
 
     return "OK"
 
