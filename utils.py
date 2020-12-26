44a29e0f21e17f147b3a5e170bb8229371ba1f87
sumingwei <46164371+SuMingWei@users.noreply.github.com>
2020-12-27 04:05:43 +0800

    [motify] restaurant restaurant

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ButtonsTemplate,ImageSendMessage,CarouselTemplate,CarouselColumn
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ButtonsTemplate,ImageSendMessage,CarouselTemplate,CarouselColumn,URITemplateAction
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
